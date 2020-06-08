f4bc8b10acc1e94fb06f93252562ba1259a0e113
Roman Zacharek <zacharekedward@gmail.com>
2020-06-08 21:48:28 +0200

    Compiles but didn't pass a test case

--- Main.java
+++ Main.java
@@ -1,11 +1,26 @@
 package advisor;
- 
+
 import java.util.Scanner;
- 
+
 public class Main {
-    public static void main(final String[] args) { 
-        final Scanner scanner = new Scanner(System.in);
+
+    static void authorized() {
+        Scanner scanner = new Scanner(System.in);
+        String url = "https://accounts.spotify.com/authorize?" +
+                "client_id=38e1d17aa75d464785c5849bd72342d2&redirect_uri=http://localhost:8080&response_type=code";
         String input = scanner.nextLine();
+
+        while(  input!= "auth"){
+            if(input.contains("exit")) exit();
+            System.out.println("Please, provide access for application.\n");
+            input = scanner.nextLine();
+        }
+    }
+
+    public static void main(String[] args) { 
+        Scanner scatter = new Scanner(System.in);
+        String input = scatter.nextLine();
+        authorized();
         String[] result = input.split(" ");
         while(!result[0].equals("exit")){
             System.out.println(result[0]);
@@ -13,14 +28,14 @@ public class Main {
             if(result[0].equals("featured")) { featured_();}
             if(result[0].equals("categories")) {categories_();}
             if(result[0].equals("playlists")) { playlists_();}
- 
-            input = scanner.nextLine();
+
+            input = scatter.nextLine();
             result = input.split(" ");
- 
+
         }
         exit();
     }
- 
+
     public static void new_(){
         System.out.println("---NEW RELEASES---\n" +
                 "Mountains [Sia, Diplo, Labrinth]\n" +
@@ -28,7 +43,7 @@ public class Main {
                 "The Greatest Show [Panic! At The Disco]\n" +
                 "All Out Life [Slipknot]");
     }
- 
+
     public static void featured_(){
         System.out.println("---FEATURED---\n" +
                 "Mellow Morning\n" +
@@ -36,7 +51,7 @@ public class Main {
                 "Monday Motivation\n" +
                 "Songs to Sing in the Shower");
     }
- 
+
     public static void categories_(){
         System.out.println("---CATEGORIES---\n" +
                 "Top Lists\n" +
@@ -44,7 +59,7 @@ public class Main {
                 "Mood\n" +
                 "Latin");
     }
- 
+
     public static void playlists_(){
         System.out.println("---MOOD PLAYLISTS---\n" +
                         "Walk Like A Badass  \n" +
@@ -55,6 +70,7 @@ public class Main {
     public static void exit(){
         System.out.println("---GOODBYE!---");
     }
+
     
     
 }
