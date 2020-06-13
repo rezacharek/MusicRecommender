b6737c5f256863cca93eb912f64012c897ed6861
Roman Zacharek <zacharekedward@gmail.com>
2020-06-13 22:39:53 +0200

    Initial modifications for the http part

--- Main.java
+++ Main.java
@@ -2,10 +2,12 @@ package advisor;
 
 import java.util.Scanner;
 import static java.lang.System.exit;
+import com.sun.net.httpserver;
 
 public class Main {
 
-    static void authorized() {
+    static void authorized()
+    {
         Scanner scanner = new Scanner(System.in);
         String url = "https://accounts.spotify.com/authorize?" +
                 "client_id=38e1d17aa75d464785c5849bd72342d2&redirect_uri=http://localhost:8080&response_type=code";
@@ -21,7 +23,26 @@ public class Main {
         System.out.println("---SUCCESS---\n");
     }
 
-    public static void main(String[] args) { 
+    public static void create_server(int portNumber)
+    {
+        HttpServer server = HttpServer.create();
+        server.bind(new InetSocketddress(portNumber),0);
+
+        server.createContext("/",
+                new HttpHandler() {
+                    public void handle(HttpExchange exchange) throws IOException {
+                        String hello = "hello, world";
+                        exchange.sendResponseHeaders(200, hello.length());
+                        exchange.getResponseBody().write(hello.getBytes());
+                        exchange.getResponseBody().close();
+                    }
+                }
+        );
+
+    }
+
+    public static void main(String[] args)
+    {
         Scanner scatter = new Scanner(System.in);
         authorized();
         String input = scatter.nextLine();
@@ -31,7 +52,7 @@ public class Main {
             if(result[0].equals("new")) { new_();}
             if(result[0].equals("featured")) { featured_();}
             if(result[0].equals("categories")) {categories_();}
-            if(result[0].equals("playlists")) { playlists_();}
+            if(result[0].equals("playlists")) {playlists_();}
 
             input = scatter.nextLine();
             result = input.split(" ");
@@ -40,7 +61,8 @@ public class Main {
         exit_();
     }
 
-    public static void new_(){
+    public static void new_()
+    {
         System.out.println("---NEW RELEASES---\n" +
                 "Mountains [Sia, Diplo, Labrinth]\n" +
                 "Runaway [Lil Peep]\n" +
@@ -48,7 +70,8 @@ public class Main {
                 "All Out Life [Slipknot]");
     }
 
-    public static void featured_(){
+    public static void featured_()
+    {
         System.out.println("---FEATURED---\n" +
                 "Mellow Morning\n" +
                 "Wake Up and Smell the Coffee\n" +
@@ -56,7 +79,8 @@ public class Main {
                 "Songs to Sing in the Shower");
     }
 
-    public static void categories_(){
+    public static void categories_()
+    {
         System.out.println("---CATEGORIES---\n" +
                 "Top Lists\n" +
                 "Pop\n" +
@@ -64,17 +88,17 @@ public class Main {
                 "Latin");
     }
 
-    public static void playlists_(){
+    public static void playlists_()
+    {
         System.out.println("---MOOD PLAYLISTS---\n" +
-                        "Walk Like A Badass  \n" +
-                        "Rage Beats  \n" +
-                        "Arab Mood Booster  \n" +
-                        "Sunday Stroll");
+                "Walk Like A Badass  \n" +
+                "Rage Beats  \n" +
+                "Arab Mood Booster  \n" +
+                "Sunday Stroll");
     }
-    public static void exit_(){
+    public static void exit_()
+    {
         System.out.println("---GOODBYE!---");
     }
-
-    
     
 }
